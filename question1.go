9e6423182b8125b353ca11b9745274bbc36febd2
Nima-byte <liamrahimian@icloud.com>
2023-04-23 23:18:44 +0200

    init

---
+++ question1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var multiples int
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			multiples += i
+
+		}
+	}
+	fmt.Println(multiples)
+}
